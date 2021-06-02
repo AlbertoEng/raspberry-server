8e98b63ae13737df00b2cad16ed1e9f3e59349bd
Jesus Alberto <jesyrom0825@gmail.com>
2021-06-01 22:06:27 -0500

    'segundo'

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 
 
+app.use('/',(req, res)=>{
+    res.end('Hola desde el servidors')
+})
+
+
 app.listen(3000,()=>{
     console.log('hola mundo')
 })
